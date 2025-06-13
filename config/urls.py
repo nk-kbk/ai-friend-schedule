13229cc197e9c141e99b5d53e25c9d044226f77e
nk-kbk <nakajima.dev0@gmail.com>
2025-06-13 19:10:54 +0900

    本番環境でメディアファイルを配信する設定を追加

--- config/urls.py
+++ config/urls.py
@@ -1,31 +1,32 @@
-# config/urls.py の【最終解決版】フルコード！
+# config/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
-from main_app import views as main_app_views
+# ✨ re_path と serve をインポートするよ！ ✨
+from django.urls import path, include, re_path 
+from django.views.static import serve
 
-# メディアファイルを配信するために必要なインポート
+from main_app import views as main_app_views
 from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf.urls.static import static # こっちはもう使わないから消してもOK！
 
 urlpatterns = [
-    # 各アプリのURLをインクルード
+    # (ここは今まで通り、変更なしだよ)
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('calendar/', include('calendar_app.urls')),
     path('ai/', include('ai_assistant_app.urls')),
     path('notifications/', include('notifications_app.urls')),
-    # schedule_requests_app は名前空間も指定してるから、この書き方だよ！
     path('schedule-requests/', include(('schedule_requests_app.urls', 'schedule_requests_app'), namespace='schedule_requests_app')),
-    
-    # トップレベルのURLパターン
     path('', main_app_views.landing_page_view, name='landing_page'),
     path('dashboard/', main_app_views.top_page_view, name='dashboard_top_page'),
 ]
 
-# --- ✨✨ ここが一番大事な修正ポイント！ ✨✨ ---
-# if settings.DEBUG: の条件を外して、
-# 本番環境(Render)でも /media/ へのアクセスがあった時に、
-# MEDIA_ROOT で指定したフォルダの中身を見に行くようにするおまじない！
-# これで、アップロードされた音声ファイルが聞こえるようになるはず！
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# --- ✨✨ ここが新しい、本格的な地図だよ！ ✨✨ ---
+# 本番環境でも、/media/ から始まるURLへのリクエストが来たら、
+# Djangoの serve ビューを使って、MEDIA_ROOTの中のファイルを探しに行くように、
+# はっきりと道順を教えてあげるんだ！
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]

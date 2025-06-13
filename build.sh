2d87648d1e4e13e2ac02c51de576ae2341105a52
nk-kbk <nakajima.dev0@gmail.com>
2025-06-13 18:08:46 +0900

    build.shにloaddataを追加

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ✨✨ ここに追加！ ✨✨
+# データベースの準備が全部終わった後に、初期データを流し込む！
+python manage.py loaddata initial_data.json
